3394af792d25800ec599808035dc68ee800d792c
senti <brianmv@uci.edu>
2022-09-04 15:00:55 -0700

    Minimum working front-end

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  // import the base css file to compile TailwindCSS into the application
+  import '../app.css'
+</script>
+
+<!-- Insert page content from routes here -->
+<slot />
---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome the the UC Irvine Machine Learning Repository!</h1>
+<button class="btn btn-primary">Click to get started</button>
