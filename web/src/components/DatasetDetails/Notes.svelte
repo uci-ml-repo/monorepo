329d877e21e363e05ec64b98529051b8d4ed5983
senti <brianmv@uci.edu>
2022-09-10 16:54:39 -0700

    all editing placeholders done

--- web/src/components/DatasetDetails/Notes.svelte
+++ web/src/components/DatasetDetails/Notes.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
   export let ID = 0
 
+  import { clickOutside } from '$lib/actions'
+
   import { useQuery } from '@sveltestack/svelte-query'
   import trpc from '$lib/trpc'
 
   import PencilIcon from '$components/Icons/Pencil.svelte'
+  import XIcon from '$components/Icons/X.svelte'
 
   const notesQuery = useQuery(
     ['notes.getByDatasetId', ID],
@@ -12,18 +15,33 @@
   )
 
   $: notes = $notesQuery.data
+
+  let noteEditOpen = false
+
+  const openNoteEdit = () => (noteEditOpen = true)
+  const closeNoteEdit = () => (noteEditOpen = false)
 </script>
 
 <div class="flex flex-col gap-2">
   <div class="flex gap-4 items-center">
     <h1 class="text-primary text-2xl font-semibold">Notes</h1>
-    <button class="btn btn-ghost btn-circle btn-sm fill-accent">
-      <PencilIcon />
-    </button>
+    {#if noteEditOpen}
+      <button class="btn btn-error btn-circle btn-sm fill-accent" on:click={closeNoteEdit}>
+        <XIcon />
+      </button>
+    {:else}
+      <button class="btn btn-ghost btn-circle btn-sm fill-accent" on:click={openNoteEdit}>
+        <PencilIcon />
+      </button>
+    {/if}
   </div>
   {#if notes}
     <p>{notes}</p>
   {:else}
     <p class="text-xl">N/A</p>
   {/if}
+</div>
+
+<div class="modal" class:modal-open={noteEditOpen}>
+  <div class="modal-box" use:clickOutside on:outside_click={closeNoteEdit}>Edit Notes</div>
 </div>
