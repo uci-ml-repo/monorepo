c5ba3d34c0d73dbc643a8b604de01d6b92eaf111
Abdelrahman <avalonsallam@gmail.com>
2022-09-10 19:05:24 -0700

    add basic profile page layout and fill one card

---
+++ web/src/components/Icons/Database.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let extra_classes = 'h-6'
+  export { extra_classes as class }
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 24 24"
+  fill="currentColor"
+  class={extra_classes}
+>
+  <path
+    fill-rule="evenodd"
+    d="M2 20h20v-4H2v4zm2-3h2v2H4v-2zM2 4v4h20V4H2zm4 3H4V5h2v2zm-4 7h20v-4H2v4zm2-3h2v2H4v-2z"
+    clip-rule="evenodd"
+  />
+</svg>
